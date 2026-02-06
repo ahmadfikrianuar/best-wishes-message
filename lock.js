d32ed99db304c8d36d5513294f853a0617f30bdf
ahmadfikrianuar <56666791+ahmadfikrianuar@users.noreply.github.com>
2026-02-06 11:21:13 +0800

    Add files via upload
    Signed-off-by: ahmadfikrianuar <56666791+ahmadfikrianuar@users.noreply.github.com>

---
+++ lock.js
@@ -0,0 +1,14 @@
+const unlockDate = new Date("2026-02-07T00:01:00");
+const birthdayPage = "birthday.html";
+
+function checkUnlock() {
+  if (new Date() >= unlockDate) {
+    window.location.href = birthdayPage;
+  }
+}
+
+// Check immediately
+checkUnlock();
+
+// Check every second (auto redirect)
+setInterval(checkUnlock, 1000);
